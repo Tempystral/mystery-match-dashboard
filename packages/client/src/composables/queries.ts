31db879a4338eccee324ccd3c4af6a091dfb2bca
Tempystral <elijah.doern@gmail.com>
2024-04-22 16:25:51 -0400

    Add player query with optional parameters

--- packages/client/src/composables/queries.ts
+++ packages/client/src/composables/queries.ts
@@ -7,6 +7,21 @@ import {
   PlayerResponse,
   PlayerSearchParams,
 } from "@mmd/common";
+import { skipToken, useQuery } from "@tanstack/vue-query";
+import { MaybeRef } from "vue";
+
+export function usePlayerQuery<T = PlayerResponse[]>(options?: {
+  params?: PlayerSearchParams;
+  enabled?: MaybeRef<object>;
+}) {
+  const { params, enabled } = options ?? {};
+  const keys = params ? (Object.keys(params) as (keyof PlayerSearchParams)[]) : [];
+  const query = keys.length && params ? "?" + keys.map((k) => `${k}=${params[k]}`).join("&") : "";
+  return useQuery({
+    queryFn: enabled ? () => api.get<T>(`/players${query}`, {}) : skipToken,
+    queryKey: ["players", ...keys],
+  });
+}
 
 export function usePartialPlayers() {
   return useQuery({
